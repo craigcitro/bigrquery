d8417141ea9d3fd9e00ae97274631902384d18f3
Nathan Goulding <nathan.goulding@piano.io>
2015-12-23 13:17:24 -0500

    implement set_service_token rough draft

--- R/auth.r
+++ R/auth.r
@@ -1,12 +1,5 @@
 #' @importFrom httr oauth_endpoint oauth_app oauth2.0_token
-google <- oauth_endpoint(NULL, "auth", "token",
-  base_url = "https://accounts.google.com/o/oauth2")
-bigqr <- oauth_app("google",
-  "465736758727.apps.googleusercontent.com",
-  "fJbIIyoIag0oA6p114lwsV2r")
-
 bq_env <- new.env(parent = emptyenv())
-
 
 #' Get and set access credentials
 #'
@@ -27,11 +20,8 @@ bq_env <- new.env(parent = emptyenv())
 get_access_cred <- function() {
   cred <- bq_env$access_cred
   if (is.null(cred)) {
-    scopes <- c(
-        "https://www.googleapis.com/auth/bigquery",
-        "https://www.googleapis.com/auth/cloud-platform")
     for (f in bq_env$credential_fetchers) {
-      cred <- f(google, bigqr, scopes)
+      cred <- f()
       if (!is.null(cred)) {
         set_access_cred(cred)
         break
@@ -78,10 +68,40 @@ register_credential_fetcher <- function(f) {
 #' Set the default mechanism for fetching OAuth2 tokens, namely the
 #' "three-legged OAuth dance" (3LO). This simply asks the user to
 #' authenticate our app via a browser.
-fetch_oauth2_creds <- function(google, bigqr, scopes) {
-  oauth2.0_token(google, bigqr,
-                 scope = c(
-                   "https://www.googleapis.com/auth/bigquery",
-                   "https://www.googleapis.com/auth/cloud-platform"))
+fetch_oauth2_creds <- function() {
+  endpoint <- oauth_endpoint(NULL, "auth", "token",
+                           base_url = "https://accounts.google.com/o/oauth2")
+  app <- oauth_app("google",
+                     "465736758727.apps.googleusercontent.com",
+                     "fJbIIyoIag0oA6p114lwsV2r")
+
+  scopes <- c(
+    "https://www.googleapis.com/auth/bigquery",
+    "https://www.googleapis.com/auth/cloud-platform")
+
+  oauth2.0_token(endpoint, app, scopes)
 }
 register_credential_fetcher(fetch_oauth2_creds)
+
+fetch_service_token_creds <- function() {
+  if (!is.string(bq_env$service_token_file) || length(bq_env$service_token_file) == 0) {
+    stop("call set_service_token with a valid filename")
+  }
+
+  endpoint <- httr::oauth_endpoints("google")
+
+  secrets <- jsonlite::fromJSON(bq_env$service_token_file)
+
+  scope = "https://www.googleapis.com/auth/bigquery"
+
+  httr::oauth_service_token(endpoint, secrets, scope)
+}
+
+#' Sets the OAuth service token to use
+#'
+#' @param The token_file to use, a string
+#' @export
+set_service_token <- function(token_file) {
+  bq_env$service_token_file <- token_file
+  register_credential_fetcher(fetch_service_token_creds)
+}
